cd2985b8e80278c7d6e60a6ec470ce693543bccf
miosin <74895918+miosin@users.noreply.github.com>
2021-01-24 22:52:06 +0900

    画像切り替え処理追加

--- src/MainProgram.py
+++ src/MainProgram.py
@@ -29,9 +29,14 @@ class MainGUI(QtWidgets.QDialog):
     ファイルパス格納用
     """
 
-    imageMiniObjList = None
+    imageMiniObjList = []
     """
-    画像表示用オブジェクト格納用(ミニ画像)
+    画像表示用オブジェクト格納用配列(ミニ画像)
+    """
+
+    cvImgList = []
+    """
+    画像切り替え用配列
     """
 
     def __init__(self, parent=None):
@@ -44,13 +49,13 @@ class MainGUI(QtWidgets.QDialog):
         self.ui.setupUi(self)
 
         # 画像表示用オブジェクトを配列に格納
-        self.imageMiniObjList = {
+        self.imageMiniObjList = [
                         self.ui.InputImageMini1,
                         self.ui.InputImageMini2,
                         self.ui.InputImageMini3,
                         self.ui.InputImageMini4,
                         self.ui.InputImageMini5
-                       }
+                       ]
 
         # 初期表示に不要なオブジェクトを非表示
         for imageObj in self.imageMiniObjList:
@@ -68,22 +73,59 @@ class MainGUI(QtWidgets.QDialog):
         :param event:
         :return:
         """
+        def scale_box(img, width, height):
+            """
+            アスペクト比を維持したまま画像サイズ調整
+            :param img: 画像データ
+            :param width: 高さ
+            :param height: 幅
+            :return: 調整後画像データ
+            """
+            h, w = img.shape[:2]
+            aspect = w / h
+            if width / height >= aspect:
+                nh = height
+                nw = round(nh * aspect)
+            else:
+                nw = width
+                nh = round(nw / aspect)
+
+            dst = cv2.resize(img, dsize=(nw, nh))
+
+            return dst
 
         # マウスクリック時イベント
         if event.type() == QEvent.MouseButtonPress:
             if event.button() == Qt.LeftButton:
 
+                pushNum = None
                 # 押されたimageによって処理分岐
                 if source.objectName() == "InputImageMini1":
                     print("1")
+                    pushNum = 0
                 elif source.objectName() == "InputImageMini2":
                     print("2")
+                    pushNum = 1
                 elif source.objectName() == "InputImageMini3":
                     print("3")
+                    pushNum = 2
                 elif source.objectName() == "InputImageMini4":
                     print("4")
+                    pushNum = 3
                 elif source.objectName() == "InputImageMini5":
                     print("5")
+                    pushNum = 4
+
+                # 画像読み込みとサイズ調整
+                cv_img = cv2.cvtColor(self.cvImgList[pushNum], cv2.COLOR_BGR2RGB)
+                cv_img = scale_box(cv_img, 440, 340)
+                height, width, dim = cv_img.shape
+                bytesPerLine = dim * width
+                self.scene = QGraphicsScene()
+                self.image = QImage(cv_img.data, width, height, bytesPerLine, QImage.Format_RGB888)
+                self.item = QGraphicsPixmapItem(QPixmap.fromImage(self.image))
+                self.scene.addItem(self.item)
+                self.ui.InputImage.setScene(self.scene)
 
         return QWidget.eventFilter(self, source, event)
 
@@ -156,7 +198,7 @@ class MainGUI(QtWidgets.QDialog):
         フォルダ選択ボタン押下時処理
         :return: void
         """
-
+        
         def scale_box(img, width, height):
             """
             アスペクト比を維持したまま画像サイズ調整
@@ -210,8 +252,15 @@ class MainGUI(QtWidgets.QDialog):
 
             if fileIdx == 0:
 
-                # 画像読み込みとサイズ調整
+                # 切り替え用配列初期化
+                self.cvImgList = []
+
+                # 画像読み込み
                 cv_img = cv2.imread(files[fileIdx])
+
+                # 切り替え用配列に格納
+                self.cvImgList.append(cv_img)
+
                 cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
 
                 # 最初の一枚のみ、InputImageとImageMini両方に格納
@@ -238,6 +287,10 @@ class MainGUI(QtWidgets.QDialog):
             else:
                 # 画像読み込みとサイズ調整
                 cv_img = cv2.imread(files[fileIdx])
+
+                # 切り替え用配列に格納
+                self.cvImgList.append(cv_img)
+
                 cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
                 cv_img = scale_box(cv_img, 75, 85)
                 height, width, dim = cv_img.shape
